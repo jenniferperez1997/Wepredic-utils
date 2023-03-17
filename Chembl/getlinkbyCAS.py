5cb5493b45ec6c35c5c92edff53b1d10b2898cd7
tdarde <thomas.darde@scilicium.com>
2023-03-17 17:01:44 +0100

    Add get name

--- Chembl/getlinkbyCAS.py
+++ Chembl/getlinkbyCAS.py
@@ -8,9 +8,10 @@ with open("ChemicalList.txt") as file:
     chemical=l[0]
     CAS=l[1]
     molecule = new_client.molecule
-    mols = molecule.filter(pref_name__iexact=chemical).only(['molecule_chembl_id', 'molecule_properties', 'molecule_structures'])
+    mols = molecule.filter(pref_name__iexact=chemical).only(['molecule_chembl_id', 'molecule_properties', 'molecule_structures','cross_references'])
     mol=mols[0]
-    out = 'https://www.ebi.ac.uk/chembl/compound_report_card/'+mol['molecule_chembl_id']+'\t'+mol['molecule_properties']['full_molformula']+'\t'+mol['molecule_structures']['canonical_smiles']+'\t'+mol['molecule_structures']['standard_inchi_key']+'\t'+mol['molecule_properties']['alogp']+'\t'+mol['molecule_properties']['full_mwt']
+    print(chemical)
+    out = mol['cross_references'][0]['xref_name']+'\t'+'https://www.ebi.ac.uk/chembl/compound_report_card/'+mol['molecule_chembl_id']+'\t'+mol['molecule_properties']['full_molformula']+'\t'+mol['molecule_structures']['canonical_smiles']+'\t'+mol['molecule_structures']['standard_inchi_key']+'\t'+mol['molecule_properties']['alogp']+'\t'+mol['molecule_properties']['full_mwt']
 
     print(out)
 
